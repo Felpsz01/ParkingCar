e5e7098a78d4d0ac7ce0353de9667311349d4af1
Felpsz01 <fvs.silva12@gmail.com>
2025-09-03 17:13:22 -0300

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native';
+import styled from 'styled-components/native';
+import Login from './src/screens/login/index';
 
 export default function App() {
+  const ContainerApp = styled.SafeAreaView`
+  flex: 1;
+  background-color: '#000';
+  `
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ContainerApp>
+       <StatusBar hidden />
+      <Login />
+    </ContainerApp>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/screens/login/index.jsx
@@ -0,0 +1,10 @@
+import { Text } from "react-native";
+import { LoginContainer } from "./styles";
+
+export default function Login() {
+    return (
+        <LoginContainer>
+            <Text>Cade o texto do login</Text>
+        </LoginContainer>
+    );
+}
